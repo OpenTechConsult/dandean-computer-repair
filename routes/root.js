09fec037da35d75dbb49818765e2e361d73f00a0
OpenTech-Consult <opentech.consult@gmail.com>
2023-03-30 11:38:15 +0000

    dandean-computer-repair backend set up. installed the express and nodemon packages, created the server.js setup the express HTTP web server. Also created route to the home page and also route to a 404 page.

---
+++ routes/root.js
@@ -0,0 +1,11 @@
+const path = require('node:path');
+const express = require('express');
+const router = express.Router();
+
+
+router.get('^/$|/index(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+})
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,29 @@
+const path = require('node:path');
+const express = require("express");
+
+const rootRoute = require('./routes/root');
+
+
+const app = express();
+const PORT = process.env.PORT || 3500;
+
+app.use('/', express.static(path.join(__dirname, '/public')))
+
+app.use('/', rootRoute);
+
+
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({message: '404 Not Found'});
+    } else {
+        res.type('text').send('404 Not Found');
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
